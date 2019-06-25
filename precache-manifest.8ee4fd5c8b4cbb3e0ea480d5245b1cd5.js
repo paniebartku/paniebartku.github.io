a44e727e9e5ac2e9caad841b27bb51df9bf1cde3
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-06-25 10:30:04 +0200

    Initial commit

---
+++ precache-manifest.8ee4fd5c8b4cbb3e0ea480d5245b1cd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "edbee593b151980cecdca2bb6878b5ea",
+    "url": "/static/media/mc.edbee593.svg"
+  },
+  {
+    "revision": "a824337fdb62c53d2eb16862cf799da7",
+    "url": "/static/media/lp.a824337f.svg"
+  },
+  {
+    "revision": "c31a83dda389cf65041072da1bc1ec7c",
+    "url": "/static/media/logo2.c31a83dd.svg"
+  },
+  {
+    "revision": "9c6c1b3e95791ccef3950438fab1cbf3",
+    "url": "/static/media/cd.9c6c1b3e.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d8a4f7bdda62d476274",
+    "url": "/static/js/main.b56af8b8.chunk.js"
+  },
+  {
+    "revision": "4d1c37ae610da1b9a867",
+    "url": "/static/js/2.25f2981b.chunk.js"
+  },
+  {
+    "revision": "1f89b03127bec1a87c7cdaaf13a39c4b",
+    "url": "/index.html"
+  }
+];
